42c3afd0c87e22bce1e4950150a01ed7897a82df
ruochen <wangrui@ruochen.email>
2022-03-14 17:42:43 +0800

    Spring+Mybatis+MP实现查询

---
+++ mybatis-plus-spring/src/main/java/com/ruochen/mp/simple/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.ruochen.mp.simple.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ruochen.mp.simple.pojo.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
